19804d4882eee54c8eb0a63950c473393c979a04
Takuto Suzuki <takuto.suzuki@mapbox.com>
2022-05-08 13:09:21 +0900

    fix button css clash

--- src/lib/components/Button.tsx
+++ src/lib/components/Button.tsx
@@ -39,7 +39,7 @@ export const Button = (props: Props) => {
 
     return (
         <button
-            className={"py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center " + commonClass + bgColor}
+            className={commonClass + bgColor}
             data-testid={dataTestId}
             disabled={disabled}
             onClick={preventClick ? () => { } : onClick}
