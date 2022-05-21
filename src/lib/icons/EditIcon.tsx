c412e66ff9708962f1d4bffc1d72490a2447f192
Taku Suzuki <takuto.suzuki@mapbox.com>
2022-05-21 11:19:09 +0900

    fix errors (#53)
    * update default to staging
    
    * update endpoint to staging
    
    * update endpoint to prod
    
    * fix styles conflict
    
    * fix warnings
    
    * do not use vscode lint

--- src/lib/icons/EditIcon.tsx
+++ src/lib/icons/EditIcon.tsx
@@ -14,6 +14,6 @@ export const EditIcon = (props: IconProps) => {
             stroke={stroke}
             width={width}
             viewBox="0 0 24 24" fill="none"
-            stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+            strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
     )
 }
